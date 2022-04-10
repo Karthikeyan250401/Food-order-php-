8f71364d993c0a264cf0bdd640f9dc54a2934efd
Karthikeyan J <63460214+Karthikeyan250401@users.noreply.github.com>
2022-04-10 23:40:46 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center"> 2022 All rights reserved,Joseph restaurant, Developed by -<a href="a"> Joseph Karthick</a></p> 
+            </div>
+        </div>
+    </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,21 @@
+<?php include("../config/constants.php");?>
+
+
+<html>
+    <head>
+        <title>Food order website - homepage</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <!-- menu section -->
+        <div class="menu text-center">
+            <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin </a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="manage-food.php">Food</a></li>
+                <li><a href="manage-order">Order</a></li>
+            </ul> 
+            </div>  
+        </div>
